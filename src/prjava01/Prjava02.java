ec7345d1f67edf94758615576bbe43d916d8609d
Alumne <Alumne@192.168.17.18>
2014-04-11 11:23:31 +0200

    Fitxer de prova

---
+++ src/prjava01/Prjava02.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+
+/**
+ *
+ * @author Alumne
+ */
+public class Prjava02 {
+    
+}
